16b4d57042f319b760f48f8fe9c6dcd6304aaada
DeeFrancois <dametreuss@gmail.com>
2022-10-18 05:25:58 -0400

    image downloads work

--- Host/downloader.py
+++ Host/downloader.py
@@ -7,13 +7,26 @@
 #     print("Nope")
     # do whatever the script does
 import os
+import subprocess
+import json
 def main():
     if(os.path.exists('./data')):
         with open('./data','r') as f:
             print("BLAH")
             contents = f.readline()
-            print(contents)
+            # print(contents)
             f.close()
+        
+        jsn=json.loads(contents)
+        print(jsn)
+        for i in jsn:
+            # subprocess.run(["yt-dlp", "{}".format(i)])
+
+        # print(jsn)
+        # print(jsn[0])
+        # for i in contents:
+        #     print(i)
+
     else:
         print("HEREEWE")
 # main(arg)
